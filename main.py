d1607a12766d4873be6dee1397b51ab7a654b307
Rodrigo F. Díaz <rodrigo.diaz@unige.ch>
2018-10-30 12:21:25 -0300

    Allow savedir to be set in rundict.

--- main.py
+++ main.py
@@ -169,7 +169,8 @@ def runmcmc(configfile, nsteps=None, **kwargs):
         sampler.comment = '_'+sampler.comment
 
     # Pickle sampler to file
-    dump2pickle(sampler, rundict.get('sampler', None), multi=ncpus)
+    dump2pickle(sampler, rundict.get('sampler', None), multi=ncpus,
+                savedir=rundict.get('savedir', None))
     return sampler
 
 
